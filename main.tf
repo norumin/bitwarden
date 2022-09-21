6fe55573a046f1486b7c81b8262df34c5380caaf
cednore <cednore@norumin.com>
2022-09-21 06:53:01 -0700

    ✨ app module

--- main.tf
+++ main.tf
@@ -16,3 +16,13 @@ module "root" {
   vpc_cidr    = "10.0.0.0/16"
   vpc_subnets = 3
 }
+
+module "app" {
+  source = "./modules/bitwarden-app"
+
+  stage         = var.stage
+  subnet_id     = module.root.public_subnet_ids[0]
+  sg_ids        = module.root.app_instance_sg_ids
+  instance_type = "t4g.micro"
+  pubkey        = local.app_env_secrets.app_instance_public_key
+}
---
+++ modules/bitwarden-app/data.tf
@@ -0,0 +1,43 @@
+data "aws_ec2_instance_type" "main" {
+  instance_type = var.instance_type
+}
+
+data "aws_ami" "focal" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = [
+      "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-${data.aws_ec2_instance_type.main.supported_architectures[0]}-server-*"
+    ]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "app_instance_profile" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+    effect    = "Allow"
+    resources = ["*"]
+  }
+}
