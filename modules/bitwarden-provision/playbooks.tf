7a9dcc6dfa950fdb703614867ba2b007628d5eb6
cednore <cednore@norumin.com>
2022-09-21 07:52:08 -0700

    ✨ provision service user and app dir

--- modules/bitwarden-provision/playbooks.tf
+++ modules/bitwarden-provision/playbooks.tf
@@ -13,6 +13,7 @@ resource "null_resource" "app_instance_provisioner" {
       data.aws_region.current.name,
       var.app_instance_public_ip,
       var.app_keypair_path,
+      var.service_user_password,
     ])
   }
 
@@ -21,7 +22,8 @@ resource "null_resource" "app_instance_provisioner" {
       ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook ${path.module}/playbooks/app.yml \
         -u ubuntu \
         -i '${var.app_instance_public_ip},' \
-        --private-key ${var.app_keypair_path}
+        --private-key ${var.app_keypair_path} \
+        -e 'service_user_password=${var.service_user_password}'
     BASH
   }
 }
