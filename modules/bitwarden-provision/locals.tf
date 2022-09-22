07b8f8a6ad9311ca76a9d166099878a93b065e48
cednore <cednore@norumin.com>
2022-09-22 05:05:51 -0700

    ♻️ reuse playbook runner cmd

--- modules/bitwarden-provision/locals.tf
+++ modules/bitwarden-provision/locals.tf
@@ -5,4 +5,17 @@ locals {
     Stage  = var.stage
   }
   slug = "${var.app}-${var.module}-${var.stage}"
+
+  cmd_bitwarden_installer = <<BASH
+    ANSIBLE_NOCOWS=1 ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook ${path.module}/playbooks/bitwarden.yml \
+      -u ubuntu \
+      -i '${var.app_instance_public_ip},' \
+      --private-key ${var.app_keypair_path} \
+      -e 'app_domain=${var.domain}' \
+      -e 'ssl_server_cert=${var.cert.certificate_pem}' \
+      -e 'ssl_private_key=${var.cert.private_key_pem}' \
+      -e 'ssl_ca_cert=${var.cert.issuer_pem}' \
+      -e 'bw_installid=${var.bitwarden_installation_id}' \
+      -e 'bw_installkey=${var.bitwarden_installation_key}'
+  BASH
 }
